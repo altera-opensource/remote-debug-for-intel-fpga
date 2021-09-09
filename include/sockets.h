cca324aaaaee2d1d2a09fdf3c35e73f291b840a3
Khor, Swee Aun <swee.aun.khor@intel.com>
2021-09-09 16:55:28 +0800

    Added License information. Added 3-Clause BSD license file and license information for all source code and header files.
    Signed-off-by: Khor, Swee Aun <swee.aun.khor@intel.com>

--- include/sockets.h
+++ include/sockets.h
@@ -1,4 +1,4 @@
-// Copyright(c) 2020, Intel Corporation
+// Copyright(c) 2021, Intel Corporation
 //
 // Redistribution  and  use  in source  and  binary  forms,  with  or  without
 // modification, are permitted provided that the following conditions are met:
@@ -23,6 +23,7 @@
 // CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
+
 #pragma once
 
 #include "common.h"
